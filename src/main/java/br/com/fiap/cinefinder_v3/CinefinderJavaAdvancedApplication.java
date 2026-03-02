dc0df48afa60479ffa1a2a8c1fb70fcda2b68105
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2026-03-02 09:15:29 -0300

    git init

---
+++ src/main/java/br/com/fiap/cinefinder_v3/CinefinderJavaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cinefinder_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefinderJavaAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinefinderJavaAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/cinefinder_v3/CinefinderJavaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cinefinder_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefinderJavaAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
