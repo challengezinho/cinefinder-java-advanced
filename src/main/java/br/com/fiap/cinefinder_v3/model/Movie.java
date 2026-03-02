5ae4c95a5211f790d4f6c81f27d40cb2d2947805
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2026-03-02 15:29:07 -0300

    criar models básicos

--- src/main/java/br/com/fiap/cinefinder_v3/model/Movie.java
+++ src/main/java/br/com/fiap/cinefinder_v3/model/Movie.java
@@ -2,7 +2,6 @@ package br.com.fiap.cinefinder_v3.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
