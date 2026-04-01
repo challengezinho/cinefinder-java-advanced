419ee27324b9a22ebadd247a6ecf28ed9c2e6605
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2026-04-01 08:25:48 -0300

    movie auxiliary classes

---
+++ src/main/java/br/com/fiap/cinefinder_v3/service/MovieService.java
@@ -0,0 +1,26 @@
+package br.com.fiap.cinefinder_v3.service;
+
+import br.com.fiap.cinefinder_v3.dto.MovieResponse;
+import br.com.fiap.cinefinder_v3.model.Movie;
+import br.com.fiap.cinefinder_v3.repository.MovieRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MovieService {
+
+    private MovieRepo repo;
+
+    public Page<MovieResponse> getAll(Pageable pageable) {
+        return repo.findAll(pageable).map(MovieResponse::fromMovie);
+    }
+
+    public MovieResponse getById(Long id) {
+        return repo.findById(id)
+                .map(MovieResponse::fromMovie)
+                .orElseThrow();
+    }
+}
