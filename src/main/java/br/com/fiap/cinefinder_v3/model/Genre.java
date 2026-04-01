e10757ad62765ca49fe67d4fd930474cc9f7fb08
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2026-04-01 08:25:59 -0300

    refactoring

--- src/main/java/br/com/fiap/cinefinder_v3/model/Genre.java
+++ src/main/java/br/com/fiap/cinefinder_v3/model/Genre.java
@@ -1,22 +1,8 @@
 package br.com.fiap.cinefinder_v3.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name = "cf_genre")
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Genre {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotBlank
-    private String name;
+public enum GENRE {
+    SCI_FI,
+    ACTION,
+    DRAMA,
+    ADVENTURE
 }
--- src/main/java/br/com/fiap/cinefinder_v3/repository/GenreRepo.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.cinefinder_v3.repository;
-
-import br.com.fiap.cinefinder_v3.model.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepo extends JpaRepository<Genre, Long> {
-}
